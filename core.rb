dc4be0808baa1a312c452eec425ec6d60ce19cba
Lori Shepherd <lori.shepherd@roswellpark.org>
2021-05-12 15:17:35 -0400

    Add method to remove review in progress label if present when an issue is closed

--- core.rb
+++ core.rb
@@ -316,6 +316,9 @@ module Core
     if obj.has_key? 'action' and  obj['action'] == "opened"
       return Core.handle_new_issue(obj)
     end
+    if obj.has_key? 'action' and  obj['action'] == "closed"
+      return Core.handle_closed(obj)
+    end
     [200, 'Post handled']
   end
 
@@ -419,6 +422,26 @@ module Core
     Octokit.add_comment(Core::NEW_ISSUE_REPO, issue_number, comment)
     return "duplicate issue"
   end
+
+
+  def handle_closed(obj)
+    unless Core.is_authenticated?
+      Core.authenticate
+    end
+    issue_number = obj['issue']['number']
+    labels = Octokit.labels_for_issue(
+      CoreConfig.auth_config['issue_repo'], issue_number)
+    has_review_label = labels.find {
+      |i| i.name == CoreConfig.labels[:REVIEW_IN_PROGRESS_LABEL]
+    }
+    if has_review_label
+      Octokit.remove_label(
+        CoreConfig.auth_config['issue_repo'], issue_number,
+        CoreConfig.labels[:REVIEW_IN_PROGRESS_LABEL])
+    end
+    return [200, "Closed Issue"]
+  end
+
 
   # When you want to close an issue, use this.
   def Core.close_issue(issue_number, issue=nil)
@@ -824,7 +847,7 @@ module Core
 
       Please fix your version number. See [Bioconductor version numbers][1]
       Please also remember to run [BiocCheck::BiocCheck()][2] on your package
-      before submitting a new issue. BiocCheck will look for other 
+      before submitting a new issue. BiocCheck will look for other
       Bioconductor package requirements.
 
       [1]: http://bioconductor.org/developers/how-to/version-numbering/
